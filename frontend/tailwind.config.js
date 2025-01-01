717a44a93349913a4df94f9a37f0c3dca0471040
sathish14325 <sathishshankar631@gmail.com>
2025-01-01 15:22:11 +0530

    fix the bug

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+import daisyui from "daisyui";
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
@@ -56,5 +59,5 @@ export default {
       },
     },
   },
-  // plugins: [require("tailwindcss-animate"), require("daisyui")],
+  plugins: [tailwindcssAnimate, daisyui],
 };
