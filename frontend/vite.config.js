3f41aface27473fe93df6a00a18bb3898277f605
sathish14325 <sathishshankar631@gmail.com>
2025-01-01 15:42:21 +0530

    bug fixing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // The default output directory for Vite builds
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
