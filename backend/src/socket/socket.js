25e2ad7fcec6061f848e4b6f1efb6ae42d67e548
sathish14325 <sathishshankar631@gmail.com>
2025-01-01 16:58:01 +0530

    fix the socket.io

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -21,15 +21,15 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) {
     userSocketMap[userId] = socket.id;
-    // console.log(`user connected: UserId = ${userId}, SocketId = ${socket.id}`);
+    console.log(`user connected: UserId = ${userId}, SocketId = ${socket.id}`);
   }
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
   socket.on("disconnect", () => {
     if (userId) {
       delete userSocketMap[userId];
-      //   console.log(
-      //     `user disconnected: UserId = ${userId}, SocketId = ${socket.id}`
-      //   );
+      console.log(
+        `user disconnected: UserId = ${userId}, SocketId = ${socket.id}`
+      );
     }
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
